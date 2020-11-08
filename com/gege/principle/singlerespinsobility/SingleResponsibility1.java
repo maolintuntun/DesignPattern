39c14e3f878ed7385133abc9ba872e810efa5539
maolintuntun <diyeqx@gmail.com>
2020-11-09 00:35:58 +0800

    up

--- com/gege/principle/singlerespinsobility/SingleResponsibility1.java
+++ com/gege/principle/singlerespinsobility/SingleResponsibility1.java
@@ -1,5 +1,19 @@
 package com.gege.principle.singlerespinsobility;
 
 public class SingleResponsibility1 {
+	 public static void main(String[] args) {
+		 Vehicle vehicle = new Vehicle();
+		 
+		 vehicle.run("motor");
+		 vehicle.run("airplane");
+	 }
+}
 
+//交通工具类
+// 方式1：在run方法中违反了单一职责原则。
+// 根据交通工具不同 分解成不同的类
+class Vehicle{
+	public void run(String vehicle) {
+		System.out.println(vehicle+ " run on road...");
+	}
 }
---
+++ com/gege/principle/singlerespinsobility/SingleResponsibility2.java
@@ -0,0 +1,10 @@
+package com.gege.principle.singlerespinsobility;
+
+public class SingleResponsibility2 {
+
+}
+
+class RoadVehicle{
+	
+	public void run()
+}
